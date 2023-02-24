5ba51e12138bf874d244f14b2ef00bd9e256e3fa
Mihai <mihai.hulea@aut.utcluj.ro>
2023-02-24 21:49:25 +0200

    Work in progress.

---
+++ src/main/java/isp/lab2/Exercise1RandomArray/Exercise1RandomArray.java
@@ -0,0 +1,19 @@
+package isp.lab2.Exercise1RandomArray;
+
+public class Exercise1RandomArray {
+
+    public static int[] getArrayOfRandomNumbers(int n, int min, int max) {
+        return null;
+    }
+
+    public static int[] findMaxAndMin(int[] array) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        int[] a = getArrayOfRandomNumbers(10, 1, 100);
+        int[] mm = findMaxAndMin(a);
+        System.out.println("Min is:"+mm[0]+" Max is:"+mm[1] );
+    }
+}
---
+++ src/test/java/isp/lab2/Exercise1RandomArray/RandomArrayTest.java
@@ -0,0 +1,13 @@
+package isp.lab2.Exercise1RandomArray;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class RandomArrayTest {
+
+    public void findMaxAndMinTest() {
+        int[] array = {10, 2, 1, 4, 1, 6, 7, 8, 9, 9};
+        int[] expected = {1, 10};
+        int[] actual = Exercise1RandomArray.findMaxAndMin(array);
+        assertArrayEquals(expected, actual);
+    }
+}

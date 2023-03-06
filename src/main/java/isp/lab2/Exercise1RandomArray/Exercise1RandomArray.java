b3f1324cfeb8b53bfeecd35c9fde1ed77afc460a
Mihai <mihai.hulea@aut.utcluj.ro>
2023-03-06 22:42:25 +0200

    Fix issue with first exercise.

--- src/main/java/isp/lab2/Exercise1RandomArray/Exercise1RandomArray.java
+++ src/main/java/isp/lab2/Exercise1RandomArray/Exercise1RandomArray.java
@@ -1,9 +1,13 @@
 package isp.lab2.Exercise1RandomArray;
 
+import java.util.Random;
+
 public class Exercise1RandomArray {
 
-    public static int[] getArrayOfRandomNumbers(int min, int max) {
-        return null;
+    public static int[] getArrayOfRandomNumbers(int n) {
+        int[] array = new int[n];
+        //TODO: implement the logic to fill the array with random numbers
+        return array;
     }
 
     public static int[] findMaxAndMin(int[] array) {
@@ -12,7 +16,10 @@ public class Exercise1RandomArray {
 
     public static void main(String[] args) {
 
-        int[] a = getArrayOfRandomNumbers(1, 100);
+        Random random = new Random();
+        //genereaza numar random intre 10 si 100
+        int size = random.nextInt(91) + 10;
+        int[] a = getArrayOfRandomNumbers(size);
         int[] mm = findMaxAndMin(a);
         System.out.println("Min is:"+mm[0]+" Max is:"+mm[1] );
     }
